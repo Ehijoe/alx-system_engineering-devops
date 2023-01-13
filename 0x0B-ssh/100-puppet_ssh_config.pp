e214b9d19637d9aec654e2821e4160698a4c5fcd
Joseph Alikah <alikahjoseph@gmail.com>
2023-01-13 01:04:43 +0100

    0x0B - Fix task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # Configure client to connect to ssh without password
 
 file { '/etc/ssh/ssh_config':
-  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
   content =>
 "# SSH config file
 
 host = 18.234.107.94
 IdentityFile = ~/.ssh/school
 PasswordAuthentication = no
-"
+",
+  mode    => '0777',
 }
