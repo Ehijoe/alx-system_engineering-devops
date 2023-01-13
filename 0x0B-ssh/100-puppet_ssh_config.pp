9b914ad2a9b299a779051df43c081df06219aea1
Joseph Alikah <alikahjoseph@gmail.com>
2023-01-13 01:18:43 +0100

    0x0B - Fix task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,16 +2,16 @@
 
 include stdlib
 
-file { '/etc/ssh/ssh_config':
+file_line { 'Turn off passwd auth':
   ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Ensure no password is accepted':
-  path => '/etc/ssh/config',
-  line => 'PasswordAuthentication = no',
-}
-
-file_line { 'Ensure no password is accepted':
-  path => '/etc/ssh/config',
-  line => 'IdentityFile = ~/.ssh/school',
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
