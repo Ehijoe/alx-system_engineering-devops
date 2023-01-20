52f358ed4811e730f814e9f280c09b09236047a1
Joseph Alikah <alikahjoseph@gmail.com>
2023-01-20 12:02:20 +0100

    0x0C - Add task 100

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,46 @@
+# Install and configure nginx
+
+package { 'nginx':
+  provider => 'apt',
+}
+
+exec { "remove file":
+  path    => ['/usr/bin','/usr/sbin','/bin','/sbin'],
+  command => "rm -rf /var/www/html/*",
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => "Hello World!",
+}
+
+$conf = @("CONFIG")
+      server {
+	      listen 80 default_server;
+	      listen [::]:80 default_server;
+
+          root /var/www/html;
+
+          index index.html index.htm index.nginx-debian.html;
+
+          server_name _;
+
+          location /redirect_me {
+       	      return 301 https://www.google.com;
+	      }
+
+          location / {
+              try_files \$uri \$uri/ =404;
+          }
+      }
+      | CONFIG
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => $conf,
+}
+
+service { 'nginx':
+  ensure => running,
+  name   => "nginx",
+}
